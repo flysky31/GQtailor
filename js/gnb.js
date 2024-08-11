82ed0e29041400cef06950acfffff8fb6116668c
LeeJaMin <sweetmin33@naver.com>
2024-08-12 01:13:09 +0900

    Update gnb.js

--- js/gnb.js
+++ js/gnb.js
@@ -18,5 +18,35 @@ $(document).ready(function () {
         $('#lnb').toggleClass('menu--open');
         $('body').toggleClass('menu--open');
     });
+
+
+   $('#header .inner .navbar-nav li').mouseenter(function(){
+        $(this).addClass("on");
+        $(this).find('.sub_menu').addClass("on");
+
+      });
+      $('#header .inner .navbar-nav li').mouseleave(function(){
+          $(this).removeClass("on");
+          $(this).find('.sub_menu').removeClass("on");
+     
+          
+      });
+
+      $('#header .inner .navbar-nav').mouseenter(function(){
+        $("#header").addClass("on");
+
+      });
+      $('#header .inner .navbar-nav').mouseleave(function(){
+          $("#header").removeClass("on");
+     
+          
+      });
+
+      AOS.init();
+
+      $("#footer .top").click(function() {
+        // Scroll to the top of the page smoothly
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
 });
         
